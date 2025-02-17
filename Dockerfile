456737267a62196e296ca7f7ace39b9c65d6dcf6
simple11618 <1205750492@qq.com>
2025-02-17 11:20:16 +0800

    update install file

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,21 @@ LABEL author ="tushan<admin@mcecy.com>"
 
 WORKDIR /
 
-COPY ./docker/install.sh /install.sh
-COPY ./docker/start.sh /start.sh
-COPY ./docker/nginx.conf /nginx.conf
-COPY ./docker/default.conf /default.conf
-COPY ./docker/www.conf /www.conf
+
+
+COPY ./docker/install.sh ./docker/start.sh /
+COPY ./docker/nginx.conf /etc/nginx/nginx.conf
+COPY ./docker/default.conf /etc/nginx/http.d/default.conf
+COPY ./docker/www.conf /etc/php7/php-fpm.d/www.conf
 COPY ./docker/redis.conf /opt/redis.conf
-COPY ./docker/php.ini /php.ini
+COPY ./docker/php.ini /etc/php7/php.ini
 
 COPY . /www
+
+
+## change dir owner nginx, create images common path
+RUN mkdir /app && mkdir -p /app/public/images && chmod -R 777 /app/public/images &&   chown -R 100.101 /app /www
+
 
 #ENV SYSTEM_TYPE='Synology'
 
@@ -27,3 +33,4 @@ CMD ["/bin/bash","/start.sh"]
 #构建全平台 docker buildx create --name mybuilder --driver docker-container --use
 #构建全平台 docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64/v3 -t itushan/mtab --push .
 #构建本地镜像 docker build -t itushan/mtab .
+
--- docker/install.sh
+++ docker/install.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
 
-apk add php7 php7-pdo_mysql php7-xml php7-xmlrpc php7-openssl php7-posix php7-pcntl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-fileinfo php7-exif php7-redis php7-gd php7-pecl-imagick-dev php7-pecl-imagick php7-mysqlnd php7-mysqli php7-ctype php7-gmp php7-redis composer unzip  php7-xmlreader php7-xmlwriter php7-dom php7-fpm
-
-apk add php7-simplexml
-
-apk add php7-bcmath
-
-apk add php7-opcache
-
-apk add nginx
-
-apk add bash
-
-apk add redis
-
-apk add rsync
-
-apk add --no-cache tzdata
+apk add --no-cache php7 php7-pdo_mysql php7-xml php7-xmlrpc php7-openssl php7-posix php7-pcntl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-fileinfo php7-exif php7-redis php7-gd php7-pecl-imagick-dev php7-pecl-imagick php7-mysqlnd php7-mysqli php7-ctype php7-gmp php7-redis composer unzip  php7-xmlreader php7-xmlwriter php7-dom php7-fpm php7-simplexml php7-bcmath php7-opcache nginx bash redis rsync tzdata
 
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
@@ -48,4 +32,7 @@ rm -rf /www/docker/*
 rm -r /www/docker
 rm /www/Dockerfile
 
+
+
 dos2unix /start.sh
+
